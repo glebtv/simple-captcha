a98b746970a1601b745636034811d5431618d548
Gleb Tv <glebtv@gmail.com>
2018-05-26 17:26:25 +0300

    fix for current rails

--- lib/simple_captcha/storage/mongoid.rb
+++ lib/simple_captcha/storage/mongoid.rb
@@ -2,22 +2,22 @@ module SimpleCaptcha
   class SimpleCaptchaData
     include Mongoid::Document
     include Mongoid::Timestamps
-    
+
     store_in collection: "simple_captcha_data"
-    
+
     field :key, :type => String
     field :value, :type => String
-    
+
     class << self
       def get_data(key)
         SimpleCaptchaData.where(:key => key).first || new(:key => key)
       end
-      
+
       def remove_data(key)
         SimpleCaptchaData.where(:key => key).first.delete
         clear_old_data(1.hour.ago)
       end
-      
+
       def clear_old_data(time = 1.hour.ago)
         return unless Time === time
         SimpleCaptchaData.where(:updated_at.lt => time.utc).delete_all
--- lib/simple_captcha/version.rb
+++ lib/simple_captcha/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module SimpleCaptcha
-  VERSION = "0.7.0".freeze
+  VERSION = "0.8.0".freeze
 end
